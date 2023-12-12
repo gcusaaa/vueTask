f04ddd3d431adee51f5a7e638dc611c40eb3bae6
gcusaaa <gcusaaa@gmail.com>
2023-12-12 11:36:06 +0200

    My project, please don't clone it

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+        <div class="row" v-if="products.length">
+            <div class="prodCard" v-for="product in products" :key="product.id">
+                <div class="card-header">
+                    <h3>{{ product.prodName }}</h3>
+                </div>
+                <div class="card-body">
+                    <img :src="product.image" :alt="product.prodName" loading="lazy"  @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+                    <p :class="product.amount > 420 ? 'expensive' : 'cheap'">R{{ product.amount.toFixed(2) }}</p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" type="button">View Details</a>
+                </div>
+            </div>
+        </div>
+        <div v-else>
+          Unfortunitely, 
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data() {
+        return {
+            products:  
+            [
+                {
+                    id: 1,
+                    prodName: 'Python Crash Course',
+                    category: 'Python',
+                    amount: 400.9999,
+                    image: 'https://i.postimg.cc/MKDLBPYm/Python-Crash-Course-cover.jpg'
+                },
+                {
+                    id: 2,
+                    prodName: 'Python Programming',
+                    category: 'Python',
+                    amount: 900.4586,
+                    image: 'https://i.postimg.cc/rFcbKX5p/Python-Programming-for-Beginners-cover.jpg'
+                },
+                {
+                    id: 3,
+                    prodName: 'Mastering C++',
+                    category: 'C++',
+                    amount: 1800.896,
+                    image: 'https://i.postimg.cc/Ss9zrpfT/Mastering-C-Programming.jpg'
+                },
+            ]
+        }
+    },
+    components: {
+
+    },
+    computed() {},
+    methods: {
+        onMouseEnter() {
+            alert('Hello there')
+        },
+        onMouseLeave() {
+            alert('Bye')
+        }
+    }
 }
 </script>
 
@@ -22,5 +74,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.wrapper :is(.row) {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.prodCard {
+  width: 20rem;
+}
+.prodCard :is(>*, img){
+  width: 100%;
+  aspect-ratio: 1/1;
+}
+.expensive {
+  font-weight: bolder;
+  color: darkgreen;
+}
+.cheap {
+  color: black;
 }
 </style>
